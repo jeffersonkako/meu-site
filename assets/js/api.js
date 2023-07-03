c9bf4a46dbf05745a76e0995a43d2bb6a216b0c8
Jefferson Pessoa <jeffersondev@icloud.com>
2023-07-03 08:40:48 -0300

    v3

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/jeffersonkako/meu-site/main/data/profile.json?token=GHSAT0AAAAAACD52R2XVTDLB3J67XY7EL4IZFCHSNA';
+    const url = 'https://raw.githubusercontent.com/jeffersonkako/meu-site/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
