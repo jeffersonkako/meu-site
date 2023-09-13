b79629176323ec2c7f70f3b2463e63b469236ae8
Jefferson Pessoa <contato@jefferson.dev>
2023-09-13 15:42:11 -0300

    update v1

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,7 +24,7 @@ function updateProfileInfo(profileData) {
 
 function updateSoftSkills(profileData) {
     const softSkills = document.getElementById('profile.skills.softSkills')
-    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
+    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
 }
 
 function updateHardSkills(profileData) {
